7ecf5e16406e7c95d712fbfd233f041d8c0b10d2
JDwang <383287471@qq.com>
2016-05-11 20:49:11 +0800

    first commit

---
+++ fileTemplates/internal/Python Script.py
@@ -0,0 +1,9 @@
+#encoding=utf8
+__author__ = '${USER}'
+__date__ = 'create date: ${YEAR}-${MONTH}-${DAY}'
+import numpy as np
+import logging
+import timeit
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
+
+
