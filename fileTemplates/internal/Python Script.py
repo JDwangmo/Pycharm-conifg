a475849ef2bde6011e44e170d166cf9df957fc61
JDwang <383287471@qq.com>
2016-05-19 09:02:55 +0800

    update

--- fileTemplates/internal/Python Script.py
+++ fileTemplates/internal/Python Script.py
@@ -2,8 +2,9 @@
 __author__ = '${USER}'
 __date__ = 'create date: ${YEAR}-${MONTH}-${DAY}'
 import numpy as np
+import pandas as pd
 import logging
 import timeit
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
+logging.basicConfig(filename='${YEAR}${MONTH}${DAY}.log',filemode = 'w',format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 
 
