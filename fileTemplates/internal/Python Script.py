b36dd8d26b21a0691c2e4c91ca4f7663f5c94de6
JDwang <383287471@qq.com>
2016-05-24 15:08:19 +0800

    update

--- fileTemplates/internal/Python Script.py
+++ fileTemplates/internal/Python Script.py
@@ -10,13 +10,22 @@ config = yaml.load(file('./config.yaml'))
 config = config['']
 logging.basicConfig(filename=config['log_file_path'],filemode = 'w',format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 start_time = timeit.default_timer()
+print('='*30)
 print config['describe']
+print('='*30)
 print 'start running!'
+logging.debug('='*30)
 logging.debug(config['describe'])
+logging.debug('='*30)
 logging.debug('start running!')
+logging.debug('='*20)
+
+
 
 
 
 
 end_time = timeit.default_timer()
 print 'end! Running time:%ds!'%(end_time-start_time)
+logging.debug('='*20)
+logging.debug('end! Running time:%ds!'%(end_time-start_time))
