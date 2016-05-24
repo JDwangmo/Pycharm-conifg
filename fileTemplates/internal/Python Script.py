ea87cc0a12d36baea7a5946f5d088556aca7265c
JDwang <383287471@qq.com>
2016-05-24 13:36:21 +0800

    update

--- fileTemplates/internal/Python Script.py
+++ fileTemplates/internal/Python Script.py
@@ -5,6 +5,16 @@ import numpy as np
 import pandas as pd
 import logging
 import timeit
+import yaml
 logging.basicConfig(filename='${YEAR}${MONTH}${DAY}.log',filemode = 'w',format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
+config = yaml.load(file('./config.yaml'))
+config = config['']
+print 'start running!'
+start_time = timeit.default_timer()
+logging.debug(config['describe'])
 
 
+
+
+end_time = timeit.default_timer()
+print 'end! Running time:%ds!'%(end_time-start_time)
