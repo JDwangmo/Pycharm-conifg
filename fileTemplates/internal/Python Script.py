4386ef8386dcc185c2b8ef448380c82f03dfa16d
JDwang <383287471@qq.com>
2016-06-04 22:03:48 +0800

    update

--- fileTemplates/internal/Python Script.py
+++ fileTemplates/internal/Python Script.py
@@ -1,31 +1,4 @@
 #encoding=utf8
 __author__ = '${USER}'
 __date__ = 'create date: ${YEAR}-${MONTH}-${DAY}'
-import numpy as np
-import pandas as pd
-import logging
-import timeit
-import yaml
-config = yaml.load(file('./config.yaml'))
-config = config['']
-logging.basicConfig(filename=''.join(config['log_file_path']),filemode = 'w',format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
-start_time = timeit.default_timer()
-print('='*30)
-print config['describe']
-print('='*30)
-print 'start running!'
-logging.debug('='*30)
-logging.debug(config['describe'])
-logging.debug('='*30)
-logging.debug('start running!')
-logging.debug('='*20)
-
-
-
-
-
-
-end_time = timeit.default_timer()
-print 'end! Running time:%ds!'%(end_time-start_time)
-logging.debug('='*20)
-logging.debug('end! Running time:%ds!'%(end_time-start_time))
+__email__ = '383287471@qq.com'
