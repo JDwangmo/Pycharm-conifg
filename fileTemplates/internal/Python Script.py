d85083f456fe6d054e6e00817c81e92f94bfda2f
JDwangmo <383287471@qq.com>
2017-07-20 19:32:40 +0800

    Update Python Script.py

--- fileTemplates/internal/Python Script.py
+++ fileTemplates/internal/Python Script.py
@@ -1,5 +1,8 @@
-#encoding=utf8
+# encoding=utf8
+"""
+    Author:  '${USER}'
+    Date:    'create date: ${YEAR}-${MONTH}-${DAY}'; 'last updated date: ${YEAR}-${MONTH}-${DAY}'
+    Email:   '383287471@qq.com'
+    Describe: 
+"""
 from __future__ import print_function
-__author__ = '${USER}'
-__date__ = 'create date: ${YEAR}-${MONTH}-${DAY}'
-__email__ = '383287471@qq.com'
